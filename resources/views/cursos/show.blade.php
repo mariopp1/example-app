04d01ff4d98e6277d874a1315d099f8b78b8d7b0
mariopp1 <mariopp1@gmail.com>
2024-02-27 09:04:27 -0500

    Se agrega el proyecto example-app

---
+++ resources/views/cursos/show.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Cursos'. $curso)
+
+@section('content')
+    <h1>Bienvenido al curso <?php echo $curso;?></h1>
+</body>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'home')
+
+@section('content')
+    <h1>Bienvenido a la página pricipal</h1>
+@endsection
