f04ea3c8d29fa26600f43d8b1a95e136711c3e4b
mariopp1 <mariopp1@gmail.com>
2024-04-24 15:15:17 -0500

    primera versión

--- database/migrations/2024_02_27_151902_create_cursos_table.php
+++ database/migrations/2024_02_27_151902_create_cursos_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('cursos', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
-            $table->timestamps();
+            $table->text('descripcion');
+            $table->text('categoria');
+            $table->timestamps(); //created_at undate_at
         });
     }
 
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('table');
     }
 };
